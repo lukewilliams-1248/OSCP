686fa74f6a8c46c8f000de01b1bcad4768ea41bd
Luke <53919295+lukewilliams-1248@users.noreply.github.com>
2020-06-06 13:03:20 -0400

    Update screenshot1.1.sh

--- screenshot1.1.sh
+++ screenshot1.1.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-#This is a script to accelerate the screenshot process, especially when pentesting.
+#This is a script to accelerate the screenshot process, especially when pentesting, by adding automatic screenshot naming
+#to imagemagick's "import" command..
 
 #It requires imagemagick for the import command:
 
 #sudo apt-get install imagemagick
-
-#Using this trades speed for specificity; as it stands, every screenshot will be
-#saved to the same folder and given a number (increasing by 1 each time) for a name.
-#This will make working through boxes faster and save you from spendning any brainpower
-#on thinking of file names, but it will slow down writing a report as your filenames won't
-#tell you much about what each screenshot is.
-#Personally, I'm more concerned about getting the points in the first place...!
 
 #I highly recommend setting up a permanent alias to use this script.
 #Edit ~/.bashrc to include:
 #alias screenshot="<path-to-this-script>/screenshot.sh
 
 #then you can take a screenshot by simply typing "screenshot" at the command line
-#and dragging a box around the area you want to capture.
-
-#of course, if you want to take a screenshot and give something a more specific name,
-#you can always use the import command as it origianlly exists!
-
-
+#and dragging a box around the area you want to capture. The first file will be named 1.png, the second 2.png, etc.
+#You can also add descriptions as an optional second argument if on your third
+#screenshot you execute with ```./screenshot1.1sh example``` it creates a file named 3_hello.png.
 
 IMAGEDIRECTORY=~/oscp/screenshots # you probably want to change this :)
 NAMEFILE=nextfile_donotdelete.txt
